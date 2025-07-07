b67a027bf0e37e05305b48cf4daed689de4db0e5
Abhishekyadav360 <163124077+Abhishekyadav360@users.noreply.github.com>
2025-07-07 16:23:57 +0530

    Update Joinnow.tsx

--- src/app/Sections/Joinnow.tsx
+++ src/app/Sections/Joinnow.tsx
@@ -48,7 +48,7 @@ const Joinnow = () => {
                     </video>
                    </div>
                     
-                        <h2 className=" font-bold text-3xl md:text-6xl text-span-gradient  !leading-none text-center z-10 max-w-4xl mx-auto " >The future of income is decentralized — and you are early.</h2>
+                        <h2 className=" font-bold text-3xl md:text-6xl text-white/90  !leading-none text-center z-10 max-w-4xl mx-auto " >The future of income is <span className='text-span-gradient'> decentralized</span>  — and you are early.</h2>
                         <p className=" text-md md:text-lg text-white  leading-tight mb-5 mt-2 text-center z-10 mx-auto"> Ready to be part of the change?
                             Click below and take the first step.</p>
                         <div className=' flex    items-center justify-center max-w-40 mx-auto bg-black z-10 rounded-xl '>
